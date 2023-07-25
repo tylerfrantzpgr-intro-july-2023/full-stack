e702f0f2567921242f9602303bef25f417721c93
tylerfrantzpgr <tyler_s_frantz@progressive.com>
2023-07-25 16:43:03 -0400

    July 25th

--- frontend/app/src/app/features/todos/state/todo-list.effects.ts
+++ frontend/app/src/app/features/todos/state/todo-list.effects.ts
@@ -1,60 +1,30 @@
-
 import { HttpClient } from '@angular/common/http';
-
 import { Injectable } from '@angular/core';
-
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-
 import { FeatureEvents } from './feature.actions';
-
 import { switchMap } from 'rxjs';
-
 import { TodoListItem } from './todo-list.reducer';
-
 import { map } from 'rxjs';
-
 import { TodoDocuments } from './todos.actions';
 
- 
-
 @Injectable()
-
 export class TodoListEffects {
-
   // When the feature is entered, go to the API, get the the todo list, and turn into a action with the items.
-
   loadItems$ = createEffect(() => {
-
     return this.actions$.pipe(
-
       ofType(FeatureEvents.featureEntered),
-
       switchMap(() =>
-
         this.httpClient
-
           .get<{ list: TodoListItem[] }>('http://localhost:8080/todo-list')
-
           .pipe(
-
             map((response) => response.list),
-
             map((payload) => TodoDocuments.todos({ payload }))
-
           )
-
       )
-
     );
-
   });
-
   constructor(
-
     private readonly actions$: Actions,
-
     private readonly httpClient: HttpClient
-
   ) {}
-
 }
--- frontend/app/src/app/features/todos/todos.routes.ts
+++ frontend/app/src/app/features/todos/todos.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { TodosComponent } from './todos.component';
-import { FEATURE_NAME, reducers } from '../counter/state';
 import { provideState } from '@ngrx/store';
+import { FEATURE_NAME, reducers } from './state';
 import { provideHttpClient } from '@angular/common/http';
+import { provideEffects } from '@ngrx/effects';
+import { TodoListEffects } from './state/todo-list.effects';
 
-export const todosRoutes: Routes = [{ path: '', component: TodosComponent, providers: [
-    provideState(FEATURE_NAME, reducers)
-] }];
+export const todosRoutes: Routes = [
+  {
+    path: '',
+    component: TodosComponent,
+    providers: [
+      provideEffects([TodoListEffects]),
+      provideState(FEATURE_NAME, reducers),
+      provideHttpClient(),
+    ],
+  },
+];
